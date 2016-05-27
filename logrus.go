e0942f1dab3b8a93c123f66b1b5deef7f8e1bce4
Gogs <gogs@fake.local>
2016-05-27 10:21:50 +0800

    init repo

---
+++ logrus.go
@@ -0,0 +1,32 @@
+package logger
+
+import (
+	"fmt"
+	"github.com/Sirupsen/logrus"
+	"time"
+)
+
+var g_defaultFormatter logrus.Formatter
+
+func init() {
+	SetDefaultFormatter()
+}
+
+func SetDefaultFormatter() {
+	if nil == g_defaultFormatter {
+		g_defaultFormatter = new(DefaultFormatter)
+	}
+	logrus.SetFormatter(g_defaultFormatter)
+}
+
+func GetComponentLogger(component string) *logrus.Entry {
+	return logrus.WithField("component", component)
+}
+
+type DefaultFormatter struct{}
+
+func (f *DefaultFormatter) Format(entry *logrus.Entry) ([]byte, error) {
+	comp, _ := entry.Data["component"].(string)
+	return []byte(fmt.Sprintf("[%s] %s _%s_ %v\n", entry.Level, entry.Time.Format(time.RFC3339Nano), comp, entry.Message)), nil
+
+}
