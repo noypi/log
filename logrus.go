dfd8f7f3a198982c4bc99a255b7f4b3059756b46
Gogs <gogs@fake.local>
2016-06-01 22:25:13 +0800

    ...

--- logrus.go
+++ logrus.go
@@ -2,41 +2,41 @@ package logger
 
 import (
 	"fmt"
-	"github.com/Sirupsen/logrus"
+	. "github.com/Sirupsen/logrus"
 	"github.com/mattn/go-colorable"
 	"time"
 )
 
-var g_defaultFormatter logrus.Formatter
+var g_defaultFormatter Formatter
 
 func init() {
 	SetDefaultFormatter()
 }
 
 func SetColoredFormatter() {
-	logrus.SetFormatter(&logrus.TextFormatter{ForceColors: true})
-	logrus.SetOutput(colorable.NewColorableStdout())
+	SetFormatter(&TextFormatter{ForceColors: true})
+	SetOutput(colorable.NewColorableStdout())
 }
 
 func EnableDebugging() {
-	logrus.SetLevel(logrus.DebugLevel)
+	SetLevel(DebugLevel)
 }
 
 func SetDefaultFormatter() {
 	if nil == g_defaultFormatter {
 		g_defaultFormatter = new(DefaultFormatter)
 	}
-	logrus.SetFormatter(g_defaultFormatter)
+	SetFormatter(g_defaultFormatter)
 }
 
-func GetComponentLogger(component string) *logrus.Entry {
-	return logrus.WithField("component", component)
+func GetComponentLogger(component string) *Entry {
+	return WithField("component", component)
 }
 
 type DefaultFormatter struct{}
 
-func (f *DefaultFormatter) Format(entry *logrus.Entry) ([]byte, error) {
+func (f *DefaultFormatter) Format(entry *Entry) ([]byte, error) {
 	comp, _ := entry.Data["component"].(string)
-	return []byte(fmt.Sprintf("[%s] %s _%s_ %v\n", entry.Level, entry.Time.Format(time.RFC3339Nano), comp, entry.Message)), nil
+	return []byte(fmt.Sprintf("[%s] %s _%s_ %v\n", entry.Level, entry.Time.Format(time.RFC3339), comp, entry.Message)), nil
 
 }
